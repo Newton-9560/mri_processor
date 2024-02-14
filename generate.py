1b4e82e1a10c8a3cfc4bd28644b6af2c6309b885
Newton-9560 <lihanwen9560@gmail.com>
2024-02-13 19:58:22 -0500

    whole

--- generate.py
+++ generate.py
@@ -9,8 +9,6 @@ if __name__ == '__main__':
     dataset_generator = DatasetGenerator('./data/mri', get_t2w_path)
     # dataset_generator.file_preprocess(convert_name)
     # dataset_generator.get_t1w_path()
-    dataset_generator.t1w_path = ['./data/mri/sub-CC00152AN04_ses-49200_desc-restore_T1w.nii.gz',
-                                  './data/mri/sub-CC00150BN02_ses-49100_desc-restore_T1w.nii.gz',
-                                  './data/mri/sub-CC00150AN02_ses-54800_desc-restore_T1w.nii.gz']
+    # dataset_generator.t1w_path = ['./data/mri/sub-CC00149XX18_ses-49000_desc-restore_T1w.nii.gz']
 
-    dataset_generator.generate_dataset('./dataset/test/')
+    dataset_generator.generate_dataset('./dataset/train/')
